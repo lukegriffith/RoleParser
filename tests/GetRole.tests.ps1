6f1cec8f68ba080b3ce1197f97f4790da0efb25d
lukemgriffith <lukemgriffith@gmail.com>
2017-08-13 10:43:48 +0100

    Adding tests

---
+++ tests/GetRole.tests.ps1
@@ -0,0 +1,46 @@
+
+
+
+Describe "Private Function - GetRole" { 
+
+    $ymlFile = gc $psscriptroot/etc/Software.yml -raw
+
+    It "test Yaml is valid." {
+        $obj = ConvertFrom-Yaml -Yaml $ymlFile -AllDocuments
+    }
+
+    Context "Well formatted YAML." {
+
+        InModuleScope -ModuleName RoleParser -ScriptBlock {
+
+            $role = GetRole -etc $psscriptroot/etc -name Software
+
+            It "GetRole gets the role structure." {
+                $role | should be $true
+            }
+
+            it "Role is root." { 
+                $role.RoleName | should be "ServerSoftware"
+            }
+
+            it "Role has children." {
+                $role.Children.Count -gt 1 | should be $true
+            }
+            
+        }
+
+    }
+
+    Context "Poor formatted YAML." {
+
+
+        InModuleScope -ModuleName RoleParser -ScriptBlock { 
+
+
+            it "Should terminate due to recursed role." {
+                { GetRole -etc $psscriptroot/etc -name SoftwareBad } | should throw
+            }
+        }
+    }
+
+}
