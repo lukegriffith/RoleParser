9ca465f21bf5aab56fbdf72835c5f6f16878061e
lukemgriffith <lukemgriffith@gmail.com>
2017-08-14 17:30:19 +0100

    refactor names

--- tests/GetRole.tests.ps1
+++ tests/GetRole.tests.ps1
@@ -4,11 +4,11 @@
 Describe "Private Function - GetRole" { 
 
 
-    Context "Well formatted YAML." {
+    Context "WellFormattedYAML." {
 
         $ymlFile = gc $psscriptroot/etc/Software.yml -raw
         
-        It "test Yaml is valid." {
+        It "YAMLIsValid." {
             { $obj = ConvertFrom-Yaml -Yaml $ymlFile -AllDocuments } | Should not throw
         }
         
@@ -17,15 +17,15 @@ Describe "Private Function - GetRole" {
 
             $role = GetRole -etc $psscriptroot/etc -name Software
 
-            It "GetRole gets the role structure." {
+            It "CorrectStructure" {
                 $role | should be $true
             }
 
-            it "Role is root." { 
+            it "IsRoot" { 
                 $role.RoleName | should be "ServerSoftware"
             }
 
-            it "Role has children." {
+            it "HasChildren" {
                 $role.Children.Count -gt 1 | should be $true
             }
             
@@ -33,18 +33,18 @@ Describe "Private Function - GetRole" {
 
     }
 
-    Context "Poor formatted YAML." {
+    Context "PoorFormattedYAML." {
         
         $ymlFile = gc $psscriptroot/etc/SoftwareBad.yml -raw
 
-        It "test Yaml is valid." {
+        It "YAMLIsValid." {
             {$obj = ConvertFrom-Yaml -Yaml $ymlFile -AllDocuments} | Should not throw
         }
 
         InModuleScope -ModuleName RoleParser -ScriptBlock { 
 
 
-            it "Should terminate due to recursed role." {
+            it "TerminatesOnRecurse" {
                 { GetRole -etc $psscriptroot/etc -name SoftwareBad } | should throw
             }
         }
